64ca4d763238a3092e20b01183865eab2af92590
HoangDung3009 <hoangdung123asd@gmail.com>
2022-03-28 14:47:02 +0700

    Modify Bottom navbar layout

--- app/src/main/java/com/example/tipun_android_app/MainActivity.java
+++ app/src/main/java/com/example/tipun_android_app/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.tipun_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button b;
+import com.example.tipun_android_app.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b = findViewById(R.id.buttonMainHome);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavBar.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.miHome:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.miSearch:
+                    replaceFragment(new SearchFragment());
+                    break;
+
+                case R.id.miFavorite:
+                    replaceFragment(new FavoriteFragment());
+                    break;
+
+                case R.id.miAccount:
+                    replaceFragment(new AccountFragment());
+                    break;
+
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
