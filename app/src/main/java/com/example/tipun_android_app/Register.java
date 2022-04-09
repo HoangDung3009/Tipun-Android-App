96e4710251fdbf23cb7de75444aedacc422f3b07
HoangDung3009 <hoangdung123asd@gmail.com>
2022-04-09 21:24:46 +0700

    add Register form

--- app/src/main/java/com/example/tipun_android_app/Register.java
+++ app/src/main/java/com/example/tipun_android_app/Register.java
@@ -2,7 +2,10 @@ package com.example.tipun_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+
+    public void navigateToLogin(View view){
+        Intent i = new Intent(Register.this, Login.class);
+        startActivity(i);
     }
 }
