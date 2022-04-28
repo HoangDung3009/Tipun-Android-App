c59ef60b73c3b467e3902eec7c04216e97dbdb51
SpringBack1905 <92899617+springback@users.noreply.github.com>
2022-04-28 23:52:54 +0700

    room details

---
+++ app/src/main/java/com/example/tipun_android_app/room_details/SliderItem.java
@@ -0,0 +1,13 @@
+package com.example.tipun_android_app.room_details;
+
+public class SliderItem {
+    private int image;
+
+    public SliderItem(int image){
+        this.image = image;
+    }
+
+    public int getImage(){
+        return  image;
+    }
+}
